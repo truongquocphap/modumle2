3ed4a5f8108428057649e2bbce543c5948ed7cb8
TRƯƠNG QUỐC PHÁP <truongquocphap@MacBook-Pro-cua-TRUONG.local>
2021-05-06 11:56:36 +0700

    upbai

--- src/bai_5_access_modifierl/thuc_hanh/Car.java
+++ src/bai_5_access_modifierl/thuc_hanh/Car.java
@@ -18,12 +18,13 @@ public class Car {
                 ", engine '" + engine + '\'' ;
     }
 
-    public static class TestStaticProperty{
-        public static void main(String[] args) {
-            Car car1=new Car("Mazda 3","Skyactiv 3");
-            System.out.println(Car.numberOfCars+" "+car1.toString());
-            Car car2=new Car("Mazda 6","Skyactiv 6");
-            System.out.println(Car.numberOfCars+" "+car2.toString());
-        }
+
+}
+ class TestStaticProperty{
+    public static void main(String[] args) {
+        Car car1=new Car("Mazda 3","Skyactiv 3");
+        System.out.println(Car.numberOfCars+" "+car1.toString());
+        Car car2=new Car("Mazda 6","Skyactiv 6");
+        System.out.println(Car.numberOfCars+" "+car2.toString());
     }
 }
